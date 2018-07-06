0106bd2e3f06529f62eb94da438fe02eb069c0d6
Xin Li <lxin@kth.se>
2018-07-06 15:08:27 +0200

    added hello_par.f90 and updated solutions.zip

---
+++ intro_lab/solutions/hello_par.f90
@@ -0,0 +1,22 @@
+program hello_par
+
+use omp_lib
+ 
+implicit none
+
+integer :: num_threads = 4
+integer thread_id
+
+call OMP_SET_NUM_THREADS(num_threads)
+
+!$omp parallel private(thread_id)
+
+    thread_id = OMP_GET_THREAD_NUM()
+
+    print '("Hello World from thread = ", i0, " with ", i0, " threads")', thread_id, num_threads
+
+!$omp end parallel
+
+print '("all done, with hopefully ", i0, " threads")', num_threads
+
+end program
